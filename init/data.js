c632fc59544c7f55f57daea89901d4d610c74cc1
ShrijalShrestha <131869418+ShrijalShrestha@users.noreply.github.com>
2024-04-18 23:07:54 +0530

    Update data.js

--- init/data.js
+++ init/data.js
@@ -8,7 +8,6 @@ const list = [
         image3:"https://www.cnet.com/a/img/resize/c7cb26e927bebaa784fb55a01e71d7fecb15d2e3/hub/2019/06/26/3f76e99d-8055-46f3-8f27-558ee276b665/20180405-ibm-q-quantum-computer-02.jpg?auto=webp&fit=crop&height=675&width=1200"
     },
     {
-<<<<<<< HEAD
         title: "Indian Cuisine",
         category: "Food",
         description: "Indian cuisine is known for its diversity and use of local spices, herbs, vegetables, and fruits. Each region has its own cuisine that reflects the demographics of the Indian subcontinent. For example, the northern region uses a lot of dairy products, while the south uses white fish curry. Staple foods of Indian cuisine include pearl millet (bājra), rice, whole-wheat flour (aṭṭa), and a variety of lentils, such as masoor (most often red lentils), tuer (pigeon peas), urad (black gram), and moong (mung beans). Lentils may be used whole, dehusked—for example, dhuli moong or dhuli urad—or split. Split lentils, or dal, are used extensively. Some pulses, such as channa or cholae (chickpeas), rajma (kidney beans), and lobiya (black-eyed peas) are very common, especially in the northern regions. Channa and moong are also processed into flour (besan). The most important and frequently used spices and flavourings in Indian cuisine are whole or powdered chilli pepper (mirch, introduced by the Portuguese from Mexico in the 16th century), black mustard seed (sarso), cardamom (elaichi), cumin (jeera), turmeric (haldi), asafoetida (hing), ginger (adrak), coriander (dhania), and garlic (lasoon).",   
@@ -23,22 +22,23 @@ const list = [
         image1:"https://blog-images-1.pharmeasy.in/2018/05/24080015/PE_Blog29.png",
         image2:"https://assets-global.website-files.com/5ee7039040ea6efb80d7521c/5fec543f57cf9ed51bab411e_image6-2.jpg",
         image3:"https://static.toiimg.com/thumb/resizemode-4,width-1280,height-720,msid-71539299/71539299.jpg"
-=======
+    },
+    {
         title: "Generative AI",
         category: "Tech",
         description: "Generative AI, or GenAI, is a type of artificial intelligence that uses user prompts to create new content like text, images, videos, sounds, code, 3D designs, and more. GenAI models combine AI algorithms to process content. For example, natural language processing techniques can transform characters into sentences, parts of speech, entities, and actions. GenAI is trained on documents and artifacts that already exist online. The goal is to produce content that has an understanding of humanlike concepts. For example, GenAI systems can be used to develop synthetic data as an alternative to data produced by real-world events. This data can be used to validate mathematical models and train machine learning models while preserving user privacy. GenAI is powered by foundation models, which are large AI models that can multi-task and perform out-of-the-box tasks, including summarization, Q&A, classification, and more. Foundation models can be adapted for targeted use cases with very little example data.",   
         image1:"https://www.ibm.com/blog/wp-content/uploads/2023/03/What-is-Generative-AI-what-are-Foundation-Models-and-why-do-they-matter-scaled.jpg",
         image2:"https://cdn.analyticsvidhya.com/wp-content/uploads/2024/03/Generative-AI-course.jpg",
         image3:"https://www.netscribes.com/wp-content/uploads/2023/08/Generative-AI-in-Healthcare.jpg"
->>>>>>> 04ae69165670f1676143139511a8783343936d63
+
     }
 ];
 
 const user = [
     {
-        name: "",
-        email: "",
-        password: ""
+        name: "Shrijal Shrestha",
+        email: "shrijal_shrestha@srmap.edu.in",
+        password: "Shrijal@123"
     }
 ];
 
